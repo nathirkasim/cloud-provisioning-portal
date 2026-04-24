9c3b9dc954cf69d1a46ccb215c065690dc90aba8
nathir <nathirkasim0@gmail.com>
2026-04-24 10:08:38 +0000

    feat: add S3 remote state backend with DynamoDB locking

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = "cloud-portal-tfstate-nathirproj"
+    key            = "portal/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "cloud-portal-tf-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
